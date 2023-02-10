bbb16f7cc938a7c2f5e6d7892eadb1fe9aa52cfe
Murutu <murutupeter@gmail.com>
2023-02-10 14:51:13 +0300

    chore: implemented useSelector functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Redux</h2>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
